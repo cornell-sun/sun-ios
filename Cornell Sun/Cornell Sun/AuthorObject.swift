586d9d1840bef019f38bb46753468677ede96a4b
Mindy Lou <mml234@cornell.edu>
2017-04-23 23:45:01 -0400

    add author object and update post object

---
+++ Cornell Sun/Cornell Sun/AuthorObject.swift
@@ -0,0 +1,26 @@
+//
+//  AuthorObject.swift
+//  Cornell Sun
+//
+//  Created by Mindy Lou on 4/23/17.
+//  Copyright © 2017 cornell.sun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AuthorObject: NSObject {
+    var id: Int
+    var name: String
+    var link: String
+    var bio: String
+    var avatarLink: String
+    
+    init(id: Int, name: String, link: String, bio: String, avatarLink: String) {
+        self.id = id
+        self.name = name
+        self.link = link
+        self.bio = bio
+        self.avatarLink = avatarLink
+    }
+}
