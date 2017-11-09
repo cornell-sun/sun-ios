d45b0e5b0e1caab3a71a60bad6615641bb42574b
Aditya Dwivedi <ad629@cornell.edu>
2017-11-08 23:32:23 -0500

    Settings screen added

---
+++ Cornell Sun/Cornell Sun/SettingsTableViewCell.swift
@@ -0,0 +1,59 @@
+//
+//  SettingsTableViewCell.swift
+//  Cornell Sun
+//
+//  Created by Aditya Dwivedi on 10/27/17.
+//  Copyright © 2017 cornell.sun. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class SettingsTableViewCell: UITableViewCell {
+    var label: UILabel!
+    var secondaryView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func setupCell(setting: SettingObject) {
+        let superview = contentView
+        label = UILabel()
+        contentView.addSubview(label)
+        label.snp.makeConstraints{ (make) -> Void in
+            make.height.equalTo(20)
+            print(UIFont.labelFontSize)
+            make.left.equalTo(superview.snp.left).offset(16)
+            make.centerY.equalTo(contentView.center.y)
+        }
+        label.text = setting.settingLabel
+        switch setting.secondaryViewType {
+        case SecondaryViewType.Label:
+            let secondLabel = UILabel()
+            contentView.addSubview(secondLabel)
+            secondLabel.snp.makeConstraints { (make) -> Void in
+                make.height.equalTo(31)
+                make.right.equalTo(superview.snp.right).offset(-13.5)
+                make.centerY.equalTo(contentView.center.y)
+            }
+            secondLabel.text = setting.secondaryLabel
+            secondLabel.textAlignment = .right
+            secondLabel.font = UIFont(name: "Times New Roman", size: 22)
+        case SecondaryViewType.Toggle:
+            let secondSwitch = UISwitch()
+            contentView.addSubview(secondSwitch)
+            secondSwitch.snp.makeConstraints { (make) -> Void in
+                make.height.equalTo(31)
+                make.bottom.equalTo(superview.snp.bottom).offset(-7)
+                make.right.equalTo(superview.snp.right).offset(-14)
+            }
+            secondaryView = secondSwitch
+        default:
+            secondaryView = UIView()
+            if setting.type == .Clickable {
+                self.accessoryType = UITableViewCellAccessoryType.disclosureIndicator
+            }
+        }
+    }
+}
---
+++ Cornell Sun/Cornell Sun/SettingsViewController.swift
@@ -0,0 +1,79 @@
+//
+//  SettingsViewController.swift
+//  Cornell Sun
+//
+//  Created by Aditya Dwivedi on 10/27/17.
+//  Copyright © 2017 cornell.sun. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var tableView: UITableView!
+    var settings: [SettingObject] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Settings"
+        view.backgroundColor = .white
+        //Calling hardcoded test. To be deleted
+        if settings.count == 0 {
+            testInit()
+        }
+        // Set up table view for settings
+        tableView = UITableView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+        tableView.register(SettingsTableViewCell.self, forCellReuseIdentifier: "SettingCell")
+        tableView.tableFooterView = UIView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.addSubview(tableView)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // swiftlint:disable:next force_cast
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingCell", for: indexPath) as! SettingsTableViewCell
+        let setting = settings[indexPath.row]
+        cell.setupCell(setting: setting)
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settings.count
+    }
+
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let setting = settings[indexPath.row]
+        if setting.type == SettingType.Clickable {
+            if let next = setting.nextController {
+                navigationController?.pushViewController(next, animated: true)
+            }
+        }
+    }
+    
+    //*Test* populator for settings array. To be deleted
+    func testInit() {
+        settings.append(SettingObject(label: "Subscribe", secondary: "", next: SettingsViewController(), secType: .None))
+        let notifViewController = SettingsViewController()
+        let notifSettings = ["Breaking News", "Opinion", "Sports", "Arts and Entertainment", "Science", "Dining", "Multimedia", "Sunspots"]
+        for i in notifSettings {
+            notifViewController.settings.append(SettingObject(label: i, secondary: "", next: nil, secType: .Toggle))
+        }
+        settings.append(SettingObject(label: "Notification", secondary: "", next: notifViewController, secType: .None))
+        settings.append(SettingObject(label: "Font Size", secondary: "Normal", next: nil, secType: .Label))
+        settings.append(SettingObject(label: "Support", secondary: "", next: SettingsViewController(), secType: .None))
+        settings.append(SettingObject(label: "Feedback", secondary: "", next: SettingsViewController(), secType: .None))
+        settings.append(SettingObject(label: "Rate", secondary: "", next: SettingsViewController(), secType: .None))
+        settings.append(SettingObject(label: "About", secondary: "", next: SettingsViewController(), secType: .None))
+        settings.append(SettingObject(label: "Privacy Policy", secondary: "", next: SettingsViewController(), secType: .None))
+        settings.append(SettingObject(label: "Use Agreement", secondary: "", next: SettingsViewController(), secType: .None))
+    }
+}
+
+
+
