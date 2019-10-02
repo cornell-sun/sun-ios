9fa5dbee94839116a16f6eff3e70aa53d14eeedc
Cameron Hamidi <cah376@cornell.edu>
2019-10-02 09:34:13 -0400

    Added Google AdMob initialization key to Info.plist

--- Cornell Sun/Cornell Sun/AppDelegate.swift
+++ Cornell Sun/Cornell Sun/AppDelegate.swift
@@ -87,7 +87,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         //@TODO change ad ID from test ad to our specific ID
         //our actual id ca-app-pub-4474706420182946~3782719574
         //fake id ca-app-pub-3940256099942544/2934735716
-        GADMobileAds.configure(withApplicationID: "ca-app-pub-4474706420182946~3782719574")
+//        GADMobileAds.configure(withApplicationID: "ca-app-pub-4474706420182946~3782719574")
+//        for _ in 1...50 {
+//            print("here")
+//        }
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
 
         //Set up Crashlytics
         Crashlytics.start(withAPIKey: fabricAPIKey())
